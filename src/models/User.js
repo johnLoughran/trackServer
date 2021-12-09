d534864afef6e31d865b341ca7f4b58c82f26709
John Loughran <johnpdloughran@gmail.com>
2021-12-09 10:59:04 +0000

    L189 Code not tested

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -12,4 +13,55 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+// pre save hook added to salt and hash the pwd before saving it.
+// using function(){ } instead of () => { } so that 'this' refers to the user object
+userSchema.pre('save', function() {
+  const user = this;  // rename this as user which it is anyway
+
+  // if the user has not changed their password do nothing, just move on to next() (Why?)
+  if( !user.isModified('password') ) {
+    return next();
+  }
+
+  // else, callback invoked with an error object and the hash
+  bcrypt.genSalt( 7, (err, salt) => {
+    if( err ){
+      return next( err );
+    }
+    // (else) , callback invoked with an error object and the hash
+    bcrypt.hash( user.password, salt, ( err, hash ) => {
+      if( err ) {
+        return next( err );
+      }
+      // so if no errors then save the salted hash as the pwd
+      user.password = hash;
+      // now it is saved we move on
+      next();
+    });
+  });
+
+});
+
+// create a method to compare passwords, using Promise which uses async await when called
+userSchema.methods.comparePassword = function( candidatePassword ) {
+  const user = this;
+
+  return new Promise( (resolve, reject ) => {
+    bcrypt.compare( candidatePassword, user.password, ( err, isMatch ) => {
+      // these three clauses are equivalent to if, elseif and else
+      if( err ) {
+        return reject( err );
+      }
+      if( !isMatch ) {
+        return reject( false );
+      }
+      // no need to add return as it is the last line anyway
+      resolve( true );
+    });
+  });
+});
+
+// creates a mongoose model called User based on the user Schema.
+// When we require this file we can use the User model.
+// The model has a schema and methods attached, a bit like a class.
 mongoose.model( 'User', userSchema );
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -17,16 +17,43 @@ router.post('/signup', async (req, res) => {
   try {
     const user = new User({ email, password });
 
-    await user.save(); // save user, based on User Model to mongo db in the User Collection (like a db table)
+    await user.save(); // save user, based on User Model, to mongo db in the user Collection (like a db table)
 
     const token = jwt.sign( { userId: user._id }, 'MySecretKeyString' );
     res.send( { token: token } );
-    // res.send('You made a post request to /signup');
+    // res.send('You made a post request to /signup'); // served as test text to display
   }
   catch (err) {
     // added return so that it is last thing that is done in the catch block
     return res.status(422).send( err.message );
   }
 });
+
+// Create a route to allow user to sign in (login)
+// Make the callback async to allow await for db lookup
+router.post('/signin', async ( req, res ) => {
+  // Save the email and pwd the person input from the body property of the request object
+  const { email, password } = req.body;
+
+  // If no email or no pwd was input return early with an error and a 422 status code "Sth went wrong"
+  // 401 means access forbidden
+  if( !email || !password ) {
+    return res.status(422).send( { error: { "You must enter your email and password."} } );
+  }
+  // find the user with that email, if none exists return vague error and code 401
+  const user = User.findOne( { email });
+  if( !user ) {
+    return res.status(401).send( { error: 'Invalid username or password' } );
+  }
+
+  // (else) Try to compared the pwds (this returns early if they do not match)
+  User.comparePassword( password )
+  // Make a token incorporating the user's email and secret key and send it back with the response
+
+  // If error then return with vague error and 401
+
+});
+
+
 
 module.exports = router;
